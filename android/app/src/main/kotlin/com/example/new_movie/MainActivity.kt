436624cb4e5c9540c0b29fcece1f1ad6ca6b4b28
Yeasin <realalyeasin@gmail.com>
2021-11-25 23:39:06 +0600

    tech-radio-app

---
+++ android/app/src/main/kotlin/com/example/new_movie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.new_movie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Up to Date',
+            style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 4,
+                fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.cyan[600],
+        ),
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.cyan),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: FloatingActionButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "News",
+                                    style: TextStyle(color: Colors.cyan),
+                                  ),
+                                  backgroundColor: Colors.white,
+                                ),
+                              ),
+                              //
+                              Padding(
+                                padding: const EdgeInsets.all(18.0),
+                                child: Text(
+                                  "Live Broadcast",
+                                  style:
+                                      TextStyle(fontSize: 19, letterSpacing: 4),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Icon(
+                            Icons.radio,
+                            size: 44,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "69.69 JOIN NOW!!!",
+                            style: TextStyle(fontSize: 14, letterSpacing: 4),
+                          ),
+                          SizedBox(
+                            height: 35,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Archives",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      letterSpacing: 3,
+                                      color: Colors.white,
+                                      backgroundColor: Colors.white30),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Yesterday",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      letterSpacing: 3,
+                                      color: Colors.white,
+                                      backgroundColor: Colors.white30),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Weather",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      letterSpacing: 3,
+                                      color: Colors.white,
+                                      backgroundColor: Colors.white30),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Sports",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      letterSpacing: 3,
+                                      color: Colors.white,
+                                      backgroundColor: Colors.white30),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Divider(
+                  height: 3,
+                  thickness: 2,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.lime,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                        "The Dart language is type safe; it uses static type checking to ensure that a variable’s value always matches the variable’s static type. Sometimes, this is referred to as sound typing. Although types are mandatory"),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Divider(
+                  height: 3,
+                  thickness: 2,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3.0),
+                    color: Colors.lime,
+                  ),
+                  height: 120,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Machine learning is the study of computer algorithms that can improve automatically through experience and by the use of data. It is seen as a part of artificial intelligence. Machine learning algorithms build a model based on sample data, known as training data, in order to make predictions or decisions without being explicitly programmed to do so"),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Divider(
+                  height: 3,
+                  thickness: 2,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 110,
+                  color: Colors.lime,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Data science is an interdisciplinary field that uses scientific methods, processes, algorithms and systems to extract knowledge and insights from noisy, structured and unstructured data, and apply knowledge and actionable insights from data across a broad range of application domains."),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Divider(
+                  height: 3,
+                  thickness: 2,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.lime,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Deep learning is part of a broader family of machine learning methods based on artificial neural networks with representation learning. Learning can be supervised, semi-supervised or unsupervised."),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Divider(
+                  height: 3,
+                  thickness: 2,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
+          ),
+        ),
+        drawer: Drawer(
+          elevation: 16,
+          child: Column(
+            children: [
+              Container(
+                height: 230,
+                color: Colors.cyan[200],
+                child: Padding(
+                  padding: const EdgeInsets.all(38.0),
+                  child: Image.asset(
+                    'images/r.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Divider(
+                height: 3,
+                thickness: 2,
+                color: Colors.black,
+              ),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home, color: Colors.cyan),
+              ),
+              Divider(
+                height: 3,
+                thickness: 1,
+                color: Colors.black,
+              ),
+              ListTile(
+                title: Text("Profile"),
+                leading: Icon(
+                  Icons.account_box_outlined,
+                  color: Colors.black87,
+                ),
+              ),
+              Divider(
+                height: 3,
+                thickness: 1,
+                color: Colors.black,
+              ),
+              ListTile(
+                title: Text("Favorites"),
+                leading: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              Divider(
+                height: 3,
+                thickness: 1,
+                color: Colors.black,
+              ),
+              ListTile(
+                title: Text("Settings"),
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.black87,
+                ),
+              ),
+              Divider(
+                height: 3,
+                thickness: 1,
+                color: Colors.black,
+              ),
+              ListTile(
+                title: Text("Log Out"),
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.cyan,
+                ),
+              ),
+              Divider(
+                height: 3,
+                thickness: 1,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
