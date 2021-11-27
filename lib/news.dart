8bc42babc6a82d1dac31725355aa3445461e75fc
Yeasin <realalyeasin@gmail.com>
2021-11-27 13:09:37 +0600

    tech-radio-app

--- lib/news.dart
+++ lib/news.dart
@@ -6,15 +6,213 @@ class News extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cyan[200],
-      body: Center(
-        child: Column(
+        backgroundColor: Colors.cyan[200],
+        body: ListView(
           children: [
-            Text('News', style:
-        TextStyle(fontSize: 19, letterSpacing: 4),)
+            Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {}, child: Text("COVID-19"))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                      "CDC endorses Pfizer and Moderna COVID-19 booster .."))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child:
+                                      Text("7 foods to help you sleep better"))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                      "This is the best time of day to exercise, backed by science"))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                      "27 tips to help you sleep better, starting tonight"))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                      "7 Tips and Tricks to Learn Programming Faster"))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text("Resume Writing For Internship"))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 45,
+                      color: Colors.cyan[100],
+                      child: Card(
+                        color: Colors.cyan[100],
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                      "Must Do Coding Questions Company-wise"))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                ],
+              ),
+            ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -6,15 +6,77 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cyan[200],
-      body: Center(
-        child: Column(
-          children: [
-            Text('Setings', style:
-            TextStyle(fontSize: 19, letterSpacing: 4),)
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Colors.cyan[200],
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Card(
+            color: Colors.cyan[100],
+            child: Container(
+            height: 470,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(radius: 40, backgroundImage: AssetImage('images/key1.jpg',),foregroundColor: Colors.black, backgroundColor: Colors.black,),
+                      ),
+                      SizedBox(height: 20,),
+                      Container(
+                        height: 40,
+                        width: 350,
+                        color: Colors.orange[100],
+                        child: Card(
+                          child: Center(child: Text('Change Picture')),
+                        ),
+                      ),
+                      Container(
+                        height: 40,
+                        width: 350,
+                        color: Colors.orange[100],
+                        child: Card(
+                          child: Center(child: Text('Change Username')),
+                        ),
+                      ),
+                      Container(
+                        height: 40,
+                        width: 350,
+                        color: Colors.orange[100],
+                        child: Card(
+                          child: Center(child: Text('Change Password')),
+                        ),
+                      ),
+                      Container(
+                        height: 40,
+                        width: 350,
+                        color: Colors.orange[100],
+                        child: Card(
+                          child: Center(child: Text('Change Email')),
+                        ),
+                      ),
+                      Container(
+                        height: 40,
+                        width: 350,
+                        color: Colors.orange[100],
+                        child: Card(
+                          child: Center(child: Text('Change Profile')),
+                        ),
+                      ),
+                      Container(
+                        height: 40,
+                        width: 350,
+                        color: Colors.orange[100],
+                        child: Card(
+                          child: Center(child: Text('Delete Account')),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
